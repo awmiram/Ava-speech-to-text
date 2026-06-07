98d214f97e63b46e135005b293984b3ed589f61f
awmiram <darbedarab@gmail.com>
2026-06-07 11:43:30 +0330

    final touch for main

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
-// ۱. ابزارهای اتصال ریداکس رو ایمپورت می‌کنیم
 import { Provider } from 'react-redux'
-import { store } from './store/store.jsx' // حواست به مسیرِ فایلی که ساختی باشه
+import { store } from './store/store.jsx'
 
 createRoot(document.getElementById('root')).render(
-  // ۲. کل اپلیکیشن رو داخل Provider قرار می‌دیم و store رو بهش پاس می‌دیم
   <Provider store={store}>
     <App />
   </Provider>
